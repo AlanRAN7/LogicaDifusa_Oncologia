b6fd52b410a394b28abdc22a59574e4ffafd0765
Alan Álvarez <aran.712@gmail.com>
2022-06-01 13:42:16 -0500

    Updated: results.js

--- js/results.js
+++ js/results.js
@@ -1,10 +1,14 @@
 function results() {
+  let calif_sintomas = localStorage.getItem('calif_sintomas');
   let titulo = document.getElementById("titulo");
   let diagnostico = document.getElementById("diagnostico");
   let recomendaciones = document.getElementById("recomendaciones");
   let respuesta;
 
-  if(localStorage.getItem('flag') === 'true'){
-    let selectedData = localStorage.getItem('calif_sintomas').split(',');
-  } else {}
+  if(localStorage.getItem('isSpecific') === 'true'){
+    let selectedCancer = localStorage.getItem('canceresSeleccionados').split(',');
+    respuesta  = diagnosticoEspecifico(calif_sintomas, selectedCancer);
+  } else {
+    respuesta = diagnosticoGeneral(calif_sintomas);
+  }
 }
